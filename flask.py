df81b223b490f326419d5d03315e7fb9187c230e
Rafaels Lasmanis <lasmanis.rafaels@gmail.com>
2019-12-10 08:35:50 +0200

    Update flask.py

--- flask.py
+++ flask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello World!'
 
-if __name__ == '__main__':
+if _name_ == '__main__'
     app.run(debug=True, host='0.0.0.0')
